f2488cdc321f2ce5d5d742e7d1392d037e828bbb
Pere <gpere800@gmail.com>
2023-04-05 05:49:17 -0700

    made changes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,37 +4,26 @@ import Logo from '../assests/Venlogo.jpg'
 
 function Nav() {
     return ( 
-    
-    
-    
-
-        
         <nav id = "navbar" className="flex  w-full item-center justify-between py-11 px-8 bg-blue-500 pt-2 pb-2  shadow-xl ">
-        <div>
-        <img src={Logo} className='w-9 h-9 rounded-lg'></img>
-        </div>
-          
-          <div className='flex gap-8'>
-            <div class="text-lg font-bold">
+          <div>
+            <img src={Logo} className='w-9 h-9 rounded-lg'></img>
+          </div>
+          <div className='flex md:gap-8 gap-4 md:text-lg text-sm md:font-bold font-medium'>
+            <div>
               <a href="#Home" class="text-white text-base">Home</a>
             </div>
-            <div class="text-lg font-bold">
+            <div>
               <a  href="#about" class="text-white text-base">About</a>
             </div>
-            <div class="text-lg font-bold">
+            <div>
               <a  href="#services" class="text-white text-base">Services</a>
             </div>
-            <div class="text-lg font-bold">
+            <div>
               <a  href="#contacts" class="text-white text-base">Contacts</a>
             </div>
           </div>
-         
-       
-        
-      
-      
       </nav>
-          );
+    );
 }
 
 export default Nav;
